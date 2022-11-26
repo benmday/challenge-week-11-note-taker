a589662984543e04b774d73d222b5b4b16eec85e
benmday <benjaminmichaelday@gmail.com>
2022-11-25 17:24:44 -0800

    completed all server calls

---
+++ routes/notes.js
@@ -0,0 +1,76 @@
+//imports
+const notes = require("express").Router();
+const util = require("util");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+//helper functions
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.log(`Data written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+//GET route for all notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+//GET route for specific note
+notes.get("/:id", (req, res) => {
+  const noteID = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteID);
+      return result.length > 0 ? res.json(result) : res.json("No note found");
+    });
+});
+
+//POST route
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json("Note added successfully!");
+  } else {
+    res.error("Please try again.");
+  }
+});
+
+//DELETE route
+notes.delete("/:id", (req, res) => {
+  const noteID = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteID);
+      writeToFile("./db/db.json", result);
+      res.json("Note deleted");
+    });
+});
+
+//export
+module.exports = notes;
